a88016986322ac545fe68481d6535cf0bcc0eace
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-29 14:29:28 +0000

    rsyslog 8.2504.0

--- Formula/r/rsyslog.rb
+++ Formula/r/rsyslog.rb
@@ -1,8 +1,8 @@
 class Rsyslog < Formula
   desc "Enhanced, multi-threaded syslogd"
   homepage "https://www.rsyslog.com/"
-  url "https://www.rsyslog.com/files/download/rsyslog/rsyslog-8.2412.0.tar.gz"
-  sha256 "8cdfa5a077cba576bdd6b1841cc2848b774e663b2e44a39512bb820174174802"
+  url "https://www.rsyslog.com/files/download/rsyslog/rsyslog-8.2504.0.tar.gz"
+  sha256 "5092a20ed40987c74cc604ebfcd6c749e47eb9fc34adc1c2637e6553e7f047ab"
   license all_of: ["Apache-2.0", "GPL-3.0-or-later", "LGPL-3.0-or-later"]
 
   livecheck do
